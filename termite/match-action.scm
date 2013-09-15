66017b510a6d297256cb00b426f98388e328796e
Chaos Eternal <chaos@shlug.org>
2013-09-15 15:42:06 +0800

    Initial commit of match-action and recv

---
+++ termite/match-action.scm
@@ -0,0 +1,72 @@
+;;; -*- mode: scheme; coding: utf-8; -*-
+;;;
+;;; Copyright (C) 2010, 2011, 2012 Free Software Foundation, Inc.
+;;;
+;;; This library is free software; you can redistribute it and/or
+;;; modify it under the terms of the GNU Lesser General Public
+;;; License as published by the Free Software Foundation; either
+;;; version 3 of the License, or (at your option) any later version.
+;;;
+;;; This library is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;;; Lesser General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU Lesser General Public
+;;; License along with this library; if not, write to the Free Software
+;;; Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+(define-module (termite match-action)
+  #:export (
+	    match/action
+            ))
+
+(define error (@@ (ice-9 match) error))
+;; syntax: match/action on-success on-fail exp clause1 clause2 ...
+;; in compare to original match, match/action takes two more arguments:
+;; on-success: code to insert when a clause matches, #f means do nothing
+;; on-failure: code to execute when all pattern fail, #f will raise exception as usual
+
+(define-syntax match/action
+  (syntax-rules ()
+    ((match/action)
+     (match-syntax-error "missing match expression"))
+    ((match/action on-success on-failure atom)
+     (match-syntax-error "no match clauses"))
+    ((match/action on-success on-failure (app ...) (pat . body) ...)
+     (let ((v (app ...)))
+       (match-next/action on-success on-failure v ((app ...) (set! (app ...))) (pat . body) ...)))
+    ((match/action on-success on-failure #(vec ...) (pat . body) ...)
+     (let ((v #(vec ...)))
+       (match-next/action on-success on-failure v (v (set! v)) (pat . body) ...)))
+    ((match/action on-success on-failure atom (pat . body) ...)
+     (let ((v atom))
+       (match-next/action on-success on-failure v (atom (set! atom)) (pat . body) ...)))
+    ))
+
+(define-syntax on-succ-helper
+  (syntax-rules ()
+    ((_ #f e1 ...)
+     (begin e1 ...))
+    ((_ #t e1 ...)
+     (begin e1 ...))
+    ((_ e1 e2 ...)
+     (begin e1 e2 ...))))
+
+(define-syntax match-next/action
+  (syntax-rules (=>)
+    ;; no more clauses, the match failed
+    ((match-next/action on-success on-failure v g+s)
+     (if (not on-failure) 
+	 (error 'match "no matching pattern")
+	 on-failure))
+    ;; named failure continuation
+    ((match-next/action on-success on-failure v g+s (pat (=> failure) . body) . rest)
+     (let ((failure (lambda () (match-next/action on-success on-failure v g+s . rest))))
+       ;; match-one analyzes the pattern for us
+       ((@@ (ice-9 match) match-one) v pat g+s ((@@ (ice-9 match) match-drop-ids) (on-succ-helper on-success . body)) (failure) ())))
+    ;; anonymous failure continuation, give it a dummy name
+    ((match-next/action on-success on-failure v g+s (pat . body) . rest)
+     (match-next/action on-success on-failure v g+s (pat (=> failure) . body) . rest))))
+
+
---
+++ termite/recv.scm
@@ -0,0 +1,40 @@
+
+(library (termite recv)
+(export recv
+        )
+(import (termite match-action)
+	(rnrs (6))
+	(guile) ;; throw, set-cdr!, catch 
+        (srfi srfi-18)
+	(srfi srfi-2)
+	(srfi srfi-34))
+
+	     
+
+(define-syntax  recv
+  (syntax-rules (after)
+    ((_ e1 e2 ... (after tmout on-tmout) ) (do-recv  tmout on-tmout e1 e2 ... ))
+    ((_ e1 e2 ... ) (recv e1 e2 ... (after #f #f) ))
+    )
+)
+
+
+(define-syntax do-recv
+  (syntax-rules ()
+    ((_ tmout #f e2 e3 ...)
+     (let lp ((msg (thread-receive tmout)))
+       (match/action 
+	(thread-mailbox-extract-and-rewind)
+	(lp (thread-mailbox-next tmout))
+	msg
+	e2
+	e3
+	...
+	)))
+    ((_ tmout on-tmout e2 e3 ...)
+     (catch 'timeout-reached
+       (lambda (do-recv tmout #f e2 e3 ...))
+       (lambda () on-tmout)
+       ))))
+
+)
